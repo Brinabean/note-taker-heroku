f23aae332aa478870d33745b80eb81296268e5dd
Brina <sabrinazanin97@gmail.com>
2022-05-04 20:42:21 -0400

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.urlencoded({ extended: true }));
