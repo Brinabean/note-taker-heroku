3c9afd2f7bba09c29f3f00dc56d01c654f1a33e9
Brina <sabrinazanin97@gmail.com>
2022-05-04 20:37:41 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 app.use(express.static("public"));
 
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use("/api", apiRoutes);
